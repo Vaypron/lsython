34e384d6d5577e2154065289b7c365b1666d218e
Tim <t.n.gebauer@googlemail.com>
2017-12-26 23:25:50 +0100

    Move main methode to extra file

---
+++ __main__.py
@@ -0,0 +1,28 @@
+import sys
+from lsython import generate_help, Lsython
+
+
+if __name__ == '__main__':
+    parameters = {'path': '.', 'description': False, 'suggested software': False, 'modified date': False, 'order': []}
+
+    if len(sys.argv) > 1:
+        for index, arg in enumerate(sys.argv):
+            if arg == '-h':
+                print(generate_help())
+                exit()
+            if arg[0] == '-' and 2 <= len(arg):
+                for char in arg:
+                    if char == 'd':
+                        parameters['path'] = sys.argv[index + 1]
+                    elif char == 'f':
+                        parameters['description'] = True
+                        parameters['order'].append('description')
+                    elif char == 'r':
+                        parameters['suggested software'] = True
+                        parameters['order'].append('suggested software')
+                    elif char == 'm':
+                        parameters['modified date'] = True
+                        parameters['order'].append('modified date')
+
+    active_class = Lsython(parameters=parameters)
+    print(active_class.output())
